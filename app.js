a20b65382a1d90f4963138f22b81f46a9494c098
aselleq <assel.qp@gmail.com>
2018-08-03 12:51:37 -0400

    seq; git commit -m seq

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var morgan = require('morgan');
 var express = require('express');
-const app = express()
+const app = express();
 //const router = express.Router();
+const { db } = require('./models');
+var layout = require('./views/layout.js');
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false })); 
@@ -10,8 +12,13 @@ app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
   });
 
-
   //get / 'hello world!'
   app.get("/", (req, res) => {
-    res.send("hello world!");
+    //res.send("hello world!");
+    res.send(layout())
+  })
+
+  db.authenticate().
+  then(() => {
+    console.log('connected to the database');
   })
