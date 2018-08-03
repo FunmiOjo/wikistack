400f0c6d7d4e43b623e27a2c5dee9d357b9aa4ba
aselleq <assel.qp@gmail.com>
2018-08-03 14:59:29 -0400

    some models

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 var morgan = require('morgan');
 var express = require('express');
 const app = express();
+const models = require('./models')
 //const router = express.Router();
 const { db } = require('./models');
+
 var layout = require('./views/layout.js');
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false })); 
 const PORT = 1337;
-app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`);
+
+
+
+
+const init = async () => {
+  await models.db.sync({force: true})
+  //await models.Page.sync()
+  //models.db.sync({force: true})
+  app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}!`);
   });
+}
 
-  //get / 'hello world!'
-  app.get("/", (req, res) => {
-    //res.send("hello world!");
-    res.send(layout())
-  })
+init();
 
-  db.authenticate().
-  then(() => {
-    console.log('connected to the database');
-  })
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
