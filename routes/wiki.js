766bb143aedb07e83dc07a31dd4548d8425cb368
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-08-03 16:44:31 -0400

    Worked on wiki post route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const addPage = require('../views/addPage');
+const { Page } = require("../models");
 
 router.use((req, res, next) => {
   next();
@@ -15,9 +16,28 @@ router.get('/add', (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-  res.send('post routes');
+router.post('/', async (req, res, next) => {
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
+
+  //res.send(res.json(req.body));
 });
+
+// {
+// authorName: "Funmi Ojo",
+// authorEmail: "tfunmi@funmi.com",
+// title: "Test title",
+// content: "Test content",
+// pageStatus: "status status"
+// }
 
 
 module.exports = router;
--- views/addPage.js
+++ views/addPage.js
@@ -5,25 +5,46 @@ module.exports = () => layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-    
+
     <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <label for="title" class="col-sm-2 control-label">Author</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input id="author-name" name="authorName" placeholder="Author Name" type="text" class="form-control"/>
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-10">
+        <input id="author-email" name="authorEmail" placeholder="Author Email" type="text" class="form-control"/>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" placeholder="Title" type="text" class="form-control"/>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Content</label>
+      <div class="col-sm-10">
+        <textarea id="content" name="content" placeholder="Content" type="text" class="form-control">
+        </textarea>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Status</label>
+      <div class="col-sm-10">
+        <input id="page-status" name="pageStatus" placeholder="Status" type="text" class="form-control"/>
+      </div>
+    </div>
 
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
